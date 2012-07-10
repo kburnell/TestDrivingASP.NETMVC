2ecf732e4b2101dd8edbab24fb1a51b351b12855
Keith Burnell <keith@dotnetdevdude.com>
2012-07-10 08:21:25 -0500

    Minor updates

--- TestDrivingMVC_Begin/TestDrivingMVC.Web/App_Start/RouteConfig.cs
+++ TestDrivingMVC_Begin/TestDrivingMVC.Web/App_Start/RouteConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Routing;
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
 using TestDrivingMVC.Web.Utility;
 
 namespace TestDrivingMVC.Web {
@@ -6,8 +8,14 @@ namespace TestDrivingMVC.Web {
     public class RouteConfig {
 
         public static void RegisterRoutes(RouteCollection routes) {
-
-            RouteProvider.ConfigureRoutes(routes);
+            //Out of the box
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+            routes.MapRoute(name: "Default", url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Customer", action = "Index", id = "1" },
+                namespaces: new[] { "TestDrivingMVC.Controller" });
+            //RouteProvider.ConfigureRoutes(routes);
         }
     }
 }
--- TestDrivingMVC_Begin/TestDrivingMVC.Web/Utility/RouteProvider.cs
+++ TestDrivingMVC_Begin/TestDrivingMVC.Web/Utility/RouteProvider.cs
@@ -8,8 +8,11 @@ namespace TestDrivingMVC.Web.Utility {
 
         public static RouteCollection ConfigureRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new {id = RouteParameter.Optional});
-            routes.MapRoute(name: "Default", url: "{controller}/{action}/{id}", defaults: new {controller = "Customer", action = "Index", id = "1"}, namespaces: new[] {"TestDrivingMVC.Controller"});
+            routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", 
+                defaults: new {id = RouteParameter.Optional});
+            routes.MapRoute(name: "Default", url: "{controller}/{action}/{id}", 
+                defaults: new {controller = "Customer", action = "Index", id = "1"}, 
+                namespaces: new[] {"TestDrivingMVC.Controller"});
             return routes;
         }
     }
