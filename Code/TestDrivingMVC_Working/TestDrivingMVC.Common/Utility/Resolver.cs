45197480bab5ec53b1ed6a4554c67b52b753004e
Keith Burnell <keith@dotnetdevdude.com>
2012-08-31 05:31:04 -0500

    A little early morning - pre session update
    ...what could go wrong!?!?!

---
+++ Code/TestDrivingMVC_Working/TestDrivingMVC.Common/Utility/Resolver.cs
@@ -0,0 +1,12 @@
+﻿using StructureMap;
+
+namespace TestDrivingMVC.Common.Utility {
+
+    public static class Resolver {
+
+        public static T GetConcreteInstanceOf<T>() {
+            return ObjectFactory.GetInstance<T>();
+        }
+
+    }
+}
---
+++ Code/TestDrivingMVC_Working/TestDrivingMVC.Web.Test.UI/Views/Customer/IndexTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WatiN.Core;
+
+namespace TestDrivingMVC.Web.Test.UI.Views.Customer {
+
+    [TestClass]
+    public class IndexTest {
+
+        private const string Url = "http://localhost:11074/";
+        private IE _browser;
+
+        [TestInitialize]
+        public void TestInitialize() {
+            _browser = new IE(Url);
+        }
+
+    }
+}
